71dcd8181c4367f0ca578b6d97e6e5932f8e44a9
dobson980 <dobson980@gmail.com>
2023-03-09 21:47:56 -0800

    Woman Variations

--- src/main/java/com/AiVoiceNPCs/NPCVoiceVariationMap.java
+++ src/main/java/com/AiVoiceNPCs/NPCVoiceVariationMap.java
@@ -45,6 +45,30 @@ public class NPCVoiceVariationMap {
         npcVariations.put(11032, 1); //Man
         npcVariations.put(11057, 2); //Man
         npcVariations.put(11058, 3); //Man
+        npcVariations.put(1119, 1); //Woman
+        npcVariations.put(1130, 2); //Woman
+        npcVariations.put(1131, 3); //Woman
+        npcVariations.put(1140, 4); //Woman
+        npcVariations.put(1141, 1); //Woman
+        npcVariations.put(1142, 2); //Woman
+        npcVariations.put(3015, 3); //Woman
+        npcVariations.put(3111, 4); //Woman
+        npcVariations.put(3112, 1); //Woman
+        npcVariations.put(3113, 2); //Woman
+        npcVariations.put(3268, 3); //Woman
+        npcVariations.put(3299, 4); //Woman
+        npcVariations.put(4958, 1); //Woman
+        npcVariations.put(6990, 2); //Woman
+        npcVariations.put(6991, 3); //Woman
+        npcVariations.put(6992, 4); //Woman
+        npcVariations.put(7921, 1); //Woman
+        npcVariations.put(7922, 2); //Woman
+        npcVariations.put(8863, 3); //Woman
+        npcVariations.put(8864, 4); //Woman
+        npcVariations.put(10674, 1); //Woman
+        npcVariations.put(10728, 2); //Woman
+        npcVariations.put(11053, 3); //Woman
+        npcVariations.put(11054, 4); //Woman
 
         if (npcVariations.containsKey(npcId)) {
             return npcVariations.get(npcId);
